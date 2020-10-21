9bf9d4a2691afcd5b14b1d868829a3e81a69ed74
YC <cody22004@gmail.com>
2020-10-21 10:58:53 +0800

    add UI project

---
+++ OnePageStoreUI/src/main/java/com/exapmle/OnePageStoreUI/OnePageStoreUiApplication.java
@@ -0,0 +1,13 @@
+package com.exapmle.OnePageStoreUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnePageStoreUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnePageStoreUiApplication.class, args);
+	}
+
+}
